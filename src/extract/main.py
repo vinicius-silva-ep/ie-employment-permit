ad04dc23fc0af7cc3d49ec3f0bcb184e1cfe8e8f
vinicius silva <103217120+vinicius-silva-ep@users.noreply.github.com>
2026-05-09 12:16:21 +0100

    fix: data extraction
    Before 2026 the base url was the same for all years. Now, they changed including a prefix: employment- and a sufix _1.
    
    As the scripts use the same label pattern, I had to change the main code in extraction to remove both prefix and sufix

--- src/extract/main.py
+++ src/extract/main.py
@@ -7,12 +7,12 @@ current_year = datetime.now().year
 output_dir = f"G:/My Drive/ESTUDOS DATA SCIENCE/ie-employment-permit/data/raw_data/{current_year}"
 os.makedirs(output_dir, exist_ok=True)
 
-base_url = "https://enterprise.gov.ie/en/publications/publication-files"
+base_url = "https://assets.gov.ie/static/documents" # Different base URL for the files, as they are stored in a different location on the server
 files = [
-    f"permits-by-nationality-{current_year}.xlsx",
-    f"permits-issued-to-companies-{current_year}.xlsx",
-    f"permits-by-county-{current_year}.xlsx",
-    f"permits-by-sector-{current_year}.xlsx",
+    f"686d767a/employment-permits-by-nationality-{current_year}_1.xlsx", # Note: The file names on the server have a "_1" suffix, which we will remove when saving the files locally
+    f"f24ad3ec/employment-permits-issued-to-companies-{current_year}_1.xlsx",
+    f"11a96607/employment-permits-by-county-{current_year}_1.xlsx",
+    f"94740580/employment-permits-by-sector-{current_year}_1.xlsx",
 ]
 
 for file in files:
@@ -20,8 +20,16 @@ for file in files:
     response = requests.get(url)
 
     if response.status_code == 200:
-        with open(os.path.join(output_dir, file), "wb") as f:
+        original_name = os.path.basename(file)
+        base, ext = os.path.splitext(original_name)
+        if base.endswith("_1"): # Remove the "_1" suffix if it exists 
+            base = base[:-2]
+        if base.startswith("employment-"): # Remove the "employment-" prefix if it exists
+            base = base[len("employment-"):]
+        filename = f"{base}{ext}"
+        output_path = os.path.join(output_dir, filename)
+        with open(output_path, "wb") as f:
             f.write(response.content)
-        print(f"File downloaded: {file}")
+        print(f"File downloaded: {filename}")
     else:
         print(f"Failure during the process {file} - Status: {response.status_code}")
